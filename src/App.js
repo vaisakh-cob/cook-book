5167d19b0def0421e7814913b2475bf6fc21a00d
Vaisakh Pisharody <vaisakh.cob@gmail.com>
2020-10-03 02:18:15 +0530

    feat(firestore): add firestore for data
    storing the recipes in firestore.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import RecipeList from "./RecipeList";
 import './App.css';
 
-const recipeList = [{
-  title: "Veg Pulao",
-  subTitle: "Oct 02, 2020",
-  imageUrl: "https://yummytummyrecipes.com/wp-content/uploads/2020/07/Veg-Pulav.png",
-  user: "https://i.pinimg.com/originals/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5.png",
-  description: "Pulao is a one pot rice dish made by cooking fragrant basmati rice with aromatic spices, herbs and sometimes stock."
-}];
+import { useCollectionData, firestore } from "./Firebase"
 
 function App() {
+  const recipes = firestore.collection(process.env.REACT_APP_FIRESTORE_COLLECTION_NAME);
+  const query = recipes.orderBy('createdAt');
+
+  let [ recipeList ] = useCollectionData(query);
+
+  console.log(recipeList)
+
   return (
     <div className="App">
       <header className="App-header">
-        <RecipeList recipeList={recipeList} />
+      { recipeList && <RecipeList recipeList={recipeList} /> }
       </header>
     </div>
   );
